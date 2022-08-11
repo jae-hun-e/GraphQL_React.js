2c310e2a02227eed5bf90160cff9ebcc84018da7
jae-hun-e <wognskec@hanyang.ac.kr>
2022-08-12 04:46:35 +0900

    router setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
+
 function App() {
-    return <div className="App">react + graphql</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Movies />}></Route>
+                    <Route path="/movies/:id" element={<Movie />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
