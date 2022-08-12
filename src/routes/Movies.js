3092b8979a2ec433bd2c6406bb497d4560101e6e
jae-hun-e <wognskec@hanyang.ac.kr>
2022-08-12 20:32:09 +0900

    #1.2 UseQuery

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,27 +1,28 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+// palyground에서 사용한 것과 같은 방식
+const ALL_MOVIES = gql`
+    query allMovies {
+        allMovies {
+            title
+            id
+        }
+    }
+`;
 
 export default function Movies() {
-    const [movies, setMovies] = useState([]);
-    const client = useApolloClient();
-    useEffect(() => {
-        client
-            .query({
-                query: gql`
-                    {
-                        allMovies {
-                            title
-                            id
-                        }
-                    }
-                `,
-            })
-            // .then((results) => console.log(results));
-            .then((results) => setMovies(results.data.allMovies));
-    }, [client]);
+    const result = useQuery(ALL_MOVIES);
+    console.log(result);
+    const { data, loading, error } = useQuery(ALL_MOVIES);
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
+    if (error) {
+        return <h1>fetching miss :(</h1>;
+    }
     return (
         <div>
-            {movies.map((movie) => (
+            {data.allMovies.map((movie) => (
                 <li key={movie.id}>{movie.title}</li>
             ))}
         </div>
