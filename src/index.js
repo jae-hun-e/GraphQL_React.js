43d5742df8d0786fadf20d6db792259cad0184a2
jae-hun-e <wognskec@hanyang.ac.kr>
2022-08-12 20:12:26 +0900

    #1.1 ApolloProvider

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import client from "./routes/client";
+import { ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,29 @@
-export default function Home() {
-    return <div>movies</div>;
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+export default function Movies() {
+    const [movies, setMovies] = useState([]);
+    const client = useApolloClient();
+    useEffect(() => {
+        client
+            .query({
+                query: gql`
+                    {
+                        allMovies {
+                            title
+                            id
+                        }
+                    }
+                `,
+            })
+            // .then((results) => console.log(results));
+            .then((results) => setMovies(results.data.allMovies));
+    }, [client]);
+    return (
+        <div>
+            {movies.map((movie) => (
+                <li key={movie.id}>{movie.title}</li>
+            ))}
+        </div>
+    );
 }
