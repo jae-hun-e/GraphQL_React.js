2393fb7cdb89ef872d4bf4a9c97a67a47c994b76
jae-hun-e <wognskec@hanyang.ac.kr>
2022-08-12 21:39:28 +0900

    styled settting

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,32 +1,67 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
     query getMovie($movieId: String!) {
         movie(id: $movieId) {
             id
             title
-            small_cover_img
+            medium_cover_image
+            rating
         }
     }
 `;
 
-export default function Home() {
-    const params = useParams();
-    console.log(params);
-    // const result = useQuery(GET_MOVIE, {
-    const { data, loading, error } = useQuery(GET_MOVIE, {
+export default function Movie() {
+    const { id } = useParams();
+    const { data, loading } = useQuery(GET_MOVIE, {
         variables: {
-            movieId: params.id,
+            movieId: id,
         },
     });
-    // console.log(result);
-    if (loading) {
-        return <h1> Fetching movie...</h1>;
-    }
-
-    if (error) {
-        return <h1> Fetching error :(</h1>;
-    }
-    return <h1>{data.movie.title}</h1>;
+    return (
+        <Container>
+            <Column>
+                <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+                <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+            </Column>
+            <Image bg={data?.movie?.medium_cover_image} />
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    height: 100vh;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+`;
+
+const Column = styled.div`
+    margin-left: 10px;
+    width: 50%;
+`;
+
+const Title = styled.h1`
+    font-size: 65px;
+    margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+    font-size: 35px;
+    margin-bottom: 10px;
+`;
+
+const Image = styled.div`
+    width: 25%;
+    height: 60%;
+    background-color: transparent;
+    background-image: url(${(props) => props.bg});
+    background-size: cover;
+    background-position: center center;
+    border-radius: 7px;
+`;
